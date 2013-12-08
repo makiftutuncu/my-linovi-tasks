3f8a1ed3ef41de85bdd7920a8a28765831c9c406
m.akif.tutuncu@gmail.com <m.akif.tutuncu@gmail.com@5171ad44-449a-d180-48ba-53da97d283bc>
2013-12-08 17:00:29 +0000

    Finished implementing immutable tree set with add, remove, contains, filter and map functionalities.

--- TreeSet/src/com/linovi/treeset/ImmutableTreeSet.scala
+++ TreeSet/src/com/linovi/treeset/ImmutableTreeSet.scala
@@ -1,40 +1,36 @@
 package com.linovi.treeset
 
-/** Represents a node in the ImmutableTreeSet */
-case class ImmutableNode(value: Int, left: ImmutableNode, right: ImmutableNode)
-{
-  override def toString = value.toString + " (" + hashCode() % 1000 + ")"
-}
-
-object ImmutableNode
-{
-  def apply(v: Int) = new ImmutableNode(v, null, null)
-}
-
 /** A simple implementation of an immutable binary tree */
-case class ImmutableTreeSet(root: ImmutableNode, count: Int)
+case class ImmutableTreeSet(root: ImmutableNode, size: Int)
 {
-  def +(v: Int): ImmutableTreeSet = add(v)
-  def -(v: Int): ImmutableTreeSet = remove(v)
-  def apply(v: Int): Boolean = contains(v)
-  def size = count
-  
-  /** Adds the given value as a new node to the tree */
+  // ===== ADD RELATED STUFF =====
+  /** Returns a new tree with the given value added. If value already exists, same tree will be returned. */
   def add(v: Int): ImmutableTreeSet =
   {
-    if(count == 0)
-    {
+    if(size == 0)
       ImmutableTreeSet(ImmutableNode(v), 1)
-    }
     else
     {
       findNodeToAdd(root, v) match
       {
         case Some(n) =>
         {
-          getUpdatedRootAfterAdd(v) match
+          val updateStack = findPathToUpdate(new MyStack(), root, v)
+          updateStack.pop() match
           {
-            case Some(newRoot) => ImmutableTreeSet(newRoot, count + 1)
+            case Some(nP) =>
+            {
+              if(v < nP.value)
+              {
+                val newRoot = updateNodesOnPath(updateStack, ImmutableNode(nP.value, ImmutableNode(v), nP.right))
+                ImmutableTreeSet(newRoot, size + 1)
+              }
+              else
+              {
+                val newRoot = updateNodesOnPath(updateStack, ImmutableNode(nP.value, nP.left, ImmutableNode(v)))
+                ImmutableTreeSet(newRoot, size + 1)
+              }
+            }
             
             // If somehow update fails, return this (unchanged) tree
             case None => this
@@ -46,6 +42,9 @@ case class ImmutableTreeSet(root: ImmutableNode, count: Int)
       }
     }
   }
+  
+  /** See -> add(Int): ImmutableTreeSet */
+  def +(v: Int): ImmutableTreeSet = add(v)
   
   /** Recursively finds the correct place to add the given value */
   private def findNodeToAdd(n: ImmutableNode, v: Int): Option[ImmutableNode] =
@@ -70,36 +69,18 @@ case class ImmutableTreeSet(root: ImmutableNode, count: Int)
     }
   }
   
-  /** Gets a new root by creating new nodes where necessary after an addition */
-  private def getUpdatedRootAfterAdd(v: Int): Option[ImmutableNode] =
-  {
-    var stack = findPathToUpdate(new MyStack(), root, v)
-    
-    stack.pop() match
-    {
-      case Some(nP) =>
-      {
-        var newParent = nP
-        if(v < newParent.value)
-          newParent = ImmutableNode(newParent.value, ImmutableNode(v), newParent.right)
-        else
-          newParent = ImmutableNode(newParent.value, newParent.left, ImmutableNode(v))
-        
-        Option(updateNodesOnPath(stack, newParent))
-      }
-      
-      case None => None
-    }
-  }
-  
-  /** Returns a new tree with given value deleted if it exists */
+  // ===== REMOVE RELATED STUFF =====
+  /** Returns a new tree with given value deleted. If value doesn't exist, same tree will be returned. */
   def remove(v: Int): ImmutableTreeSet =
   {
-    if(count == 0)
+    if(size == 0)
       this
     else
       removeValue(root, root, v)
   }
+  
+  /** See -> remove(Int): ImmutableTreeSet */
+  def -(v: Int): ImmutableTreeSet = remove(v)
   
   /** Traverses the tree starting from a node with it's parent and deletes the
    *  given value if it exists in the tree */
@@ -119,163 +100,187 @@ case class ImmutableTreeSet(root: ImmutableNode, count: Int)
       if(left == null && right == null)
       {
         if(v == root.value)
-        {
-          // This was root node and it was the only node, return a new empty tree
-          
-          ImmutableTreeSet()
-        }
+          ImmutableTreeSet() // This was root node and it was the only node, return a new empty tree
         else
         {
           // This was a leaf node, just update nodes starting from it's parent to root
           
-          var newParent: ImmutableNode = null
           if(v < p.value)
           {
             // Deleting from left
-            newParent = ImmutableNode(p.value, null, p.right)
+            val newParent = ImmutableNode(p.value, null, p.right)
+            ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
           }
           else
           {
             // Deleting from right
-            newParent = ImmutableNode(p.value, p.left, null)
+            val newParent = ImmutableNode(p.value, p.left, null)
+            ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
           }
-          
-          val stack = findPathToUpdate(new MyStack(), root, newParent.value)
-          val newRoot = updateNodesOnPath(stack, newParent)
-          
-          ImmutableTreeSet(newRoot, count - 1)
         }
       }
       else if(left != null && right == null)
       {
         if(v == root.value)
-        {
-          // This was root node and it only had a left subtree, return a new tree whose root is the left subtree
-          
-          ImmutableTreeSet(n.left, count - 1)
-        }
+          ImmutableTreeSet(n.left, size - 1) // This was root node and it only had a left subtree, return a new tree whose root is the left subtree
         else
         {
           // This was a node only having a left subtree, put the left subtree in deleted nodes place and update nodes starting from the parent to root
           
-          var newParent: ImmutableNode = null
           if(v < p.value)
           {
             // Deleting from left
-            newParent = ImmutableNode(p.value, left, p.right)
+            val newParent = ImmutableNode(p.value, left, p.right)
+            ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
           }
           else
           {
             // Deleting from right
-            newParent = ImmutableNode(p.value, p.left, left)
+            val newParent = ImmutableNode(p.value, p.left, left)
+            ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
           }
-          
-          val stack = findPathToUpdate(new MyStack(), root, newParent.value)
-          val newRoot = updateNodesOnPath(stack, newParent)
-          
-          ImmutableTreeSet(newRoot, count - 1)
         }
       }
       else if(left == null && right != null)
       {
         if(v == root.value)
-        {
-          // This was root node and it only had a right subtree, return a new tree whose root is the right subtree
-          
-          ImmutableTreeSet(n.right, count - 1)
-        }
+          ImmutableTreeSet(n.right, size - 1) // This was root node and it only had a right subtree, return a new tree whose root is the right subtree
         else
         {
           // This was a node only having a left subtree, put the left subtree in deleted nodes place and update nodes starting from the parent to root
           
-          var newParent: ImmutableNode = null
           if(v < p.value)
           {
             // Deleting from left
-            newParent = ImmutableNode(p.value, right, p.right)
+            val newParent = ImmutableNode(p.value, right, p.right)
+            ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
           }
           else
           {
             // Deleting from right
-            newParent = ImmutableNode(p.value, p.left, right)
+            val newParent = ImmutableNode(p.value, p.left, right)
+            ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
           }
-          
-          val stack = findPathToUpdate(new MyStack(), root, newParent.value)
-          val newRoot = updateNodesOnPath(stack, newParent)
-          
-          ImmutableTreeSet(newRoot, count - 1)
         }
       }
       else
       {
-        if(v == root.value)
+        findNextNodeToReplace(n.right) match
         {
-          // This was root node and it had both a left and a right subtree
-          // Return a new tree whose root is the left subtree and root of the right subtree added to the resulting tree
-          
-          val newTree = ImmutableTreeSet(left, count - 1)
-          val newRoot = newTree.addNode(right)
-          
-          ImmutableTreeSet(newRoot, newTree.size)
-        }
-        else
-        {
-          // This was a node having both a left subtree and a right subtree
-          // Put the left subtree in deleted nodes place, add the right subtree to the resulting tree and update nodes to the root
-          
-          val newTree = ImmutableTreeSet(left, count - 1)
-          val newSubRoot = newTree.addNode(right)
-          
-          var newParent: ImmutableNode = null
-          if(v < p.value)
+          case Some(nTR) =>
           {
-            // Deleting from left
-            newParent = ImmutableNode(p.value, newSubRoot, p.right)
-          }
-          else
-          {
-            // Deleting from right
-            newParent = ImmutableNode(p.value, p.left, newSubRoot)
+            val nodeToReplace = ImmutableNode(nTR.value, left, nTR.right)
+            
+            if(v == root.value)
+            {
+              // This was root node and it had both a left and a right subtree
+              // Return a new tree whose root is the minimum node on the right subtree
+              
+              ImmutableTreeSet(nodeToReplace, size - 1)
+            }
+            else
+            {
+              // This was a node having both a left subtree and a right subtree
+              // Return a new tree with the minimum node on the right subtree is placed in place of deleted node
+              
+              if(v < p.value)
+              {
+                // Deleting from left
+                val newParent = ImmutableNode(p.value, nodeToReplace, p.right)
+                ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
+              }
+              else
+              {
+                // Deleting from right
+                val newParent = ImmutableNode(p.value, p.left, nodeToReplace)
+                ImmutableTreeSet(updateNodesOnPath(findPathToUpdate(new MyStack(), root, newParent.value), newParent), size - 1)
+              }
+            }
           }
           
-          val stack = findPathToUpdate(new MyStack(), root, newParent.value)
-          val newRoot = updateNodesOnPath(stack, newParent)
-          
-          ImmutableTreeSet(newRoot, newTree.size)
+          case None => this
         }
       }        
     }
   }
   
-  /** Adds an existing node to the tree and returns the new root */
-  private def addNode(n: ImmutableNode): ImmutableNode =
+  /** Finds the next node to be placed in the place of node being deleted */
+  private def findNextNodeToReplace(n: ImmutableNode): Option[ImmutableNode] =
   {
-    // Find place to add the right subtree
-    findNodeToAdd(root, n.value) match
+    if(n == null)
+      None
+    else if(n.left == null)
+      Option(n)
+    else
+      findNextNodeToReplace(n.left)
+  }
+  
+  // ===== CONTAINS RELATED STUFF =====
+  /** Returns true if given value exists in the tree */
+  def contains(v: Int): Boolean = contains(root, v)
+  
+  /** See -> contains(Int): Boolean */
+  def apply(v: Int): Boolean = contains(v)
+  
+  /** Traverses the tree starting from a node and checks if the given value
+   *  exists in the tree */
+  private def contains(n: ImmutableNode, v: Int): Boolean =
+  {
+    if(n == null)
+      false
+    else if(v == n.value)
+      true
+    else if(v < n.value)
+      contains(n.left, v)
+    else
+      contains(n.right, v)
+  }
+  
+  // ===== FILTER RELATED STUFF =====
+  /** Returns a new tree with elements that satisfy the given condition */
+  def filter(f: (Int) => Boolean): ImmutableTreeSet = filter(this, root, f)
+  
+  /** Traverses the tree starting from a node and deletes nodes that doesn't satisfy the given condition */
+  private def filter(t: ImmutableTreeSet, n: ImmutableNode, f: (Int) => Boolean): ImmutableTreeSet =
+  {
+    if(n == null)
+      t
+    else
     {
-      case Some(nP) =>
-      {
-        var newParent: ImmutableNode = null
-        if(n.value < nP.value)
-        {
-          // Adding to left
-          newParent = ImmutableNode(nP.value, n, nP.right)
-        }
-        else
-        {
-          // Adding to right
-          newParent = ImmutableNode(nP.value, nP.left, n)
-        }
-        
-        val stack = findPathToUpdate(new MyStack(), root, newParent.value)
-        
-        updateNodesOnPath(stack, newParent)
-      }
-      
-      case None => root
+      if(!f(n.value))
+        filter(filter(t - n.value, n.left, f), n.right, f)
+      else
+        filter(filter(t, n.left, f), n.right, f)
     }
   }
   
+  // ===== MAP RELATED STUFF =====
+  /** Returns a new tree with each element having applied to the given function */
+  def map(f: (ImmutableNode) => ImmutableNode): ImmutableTreeSet = map(ImmutableTreeSet(), root, f)
+  
+  /** Traverses the tree starting from a node and applies given function to all nodes during the traversal */
+  private def map(t: ImmutableTreeSet, n: ImmutableNode, f: (ImmutableNode) => ImmutableNode): ImmutableTreeSet =
+  {
+    if(n == null)
+      t
+    else
+      map(map(t + f(n).value, n.left, f), n.right, f)
+  }
+  
+  // ===== OUTPUT RELATED STUFF =====
+  /** Traverses the tree starting from a node and prints all it's contents recursively */
+  private def print(n: ImmutableNode): String =
+  {
+    if(n == null)
+      "_"
+    else
+      "{" + n + ", " + print(n.left) + ", " + print(n.right) + "}"
+  }
+  
+  /** Gives a String representation of this tree */
+  override def toString = "Tree (" + (hashCode() % 1000) + ")\n" + print(root)
+  
+  // ===== OTHER HELPER STUFF =====
   /** Recursively finds the path to the given value's node and returns a stack of the nodes on the way */
   private def findPathToUpdate(stack: MyStack, node: ImmutableNode, v: Int): MyStack =
   {
@@ -311,34 +316,6 @@ case class ImmutableTreeSet(root: ImmutableNode, count: Int)
       }
     }
   }
-  
-  /** Returns true if given value exists in the tree */
-  def contains(v: Int): Boolean = contains(root, v)
-  
-  /** Traverses the tree starting from a node and checks if the given value
-   *  exists in the tree */
-  private def contains(n: ImmutableNode, v: Int): Boolean =
-  {
-    if(n == null)
-      false
-    else if(v == n.value)
-      true
-    else if(v < n.value)
-      contains(n.left, v)
-    else
-      contains(n.right, v)
-  }
-  
-  /** Traverses the tree starting from a node and prints it's contents */
-  private def print(n: ImmutableNode): String =
-  {
-    if(n == null)
-      "_"
-    else
-      "{" + n + ", " + print(n.left) + ", " + print(n.right) + "}"
-  }
-  
-  override def toString = "Tree (" + (hashCode() % 1000) + ")\n" + print(root)
 }
 
 object ImmutableTreeSet
